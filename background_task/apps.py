124966ae34696c55a8721cb663c18fff56f160ae
Raphaël Stefanini <rphlo@users.noreply.github.com>
2023-06-12 09:21:38 +0300

    Update apps.py

--- background_task/apps.py
+++ background_task/apps.py
@@ -3,7 +3,6 @@ from django.apps import AppConfig
 
 class BackgroundTasksAppConfig(AppConfig):
     name = "background_task"
-    from background_task import __version__ as version_info
     verbose_name = "Background Tasks"
 
     def ready(self):
